1d50b56117ea01d85a98470646d0f16092c26491
Octavio Licea Leon <olicea@github.com>
2022-01-10 07:28:20 -0500

    better sliding window

--- patterns/average-sum/Program.cs
+++ patterns/average-sum/Program.cs
@@ -5,8 +5,39 @@
         int[] array = new[] {1, 3, 2, 6, -1, 4, 1, 8, 2};
         int k=5;
 
-        double[] averages = AverageOFArrayReuse(array, k);
+        double[] averages = AverageOFArraySliding(array, k);
         Console.WriteLine($"average {string.Join(",", averages)}");
+    }
+
+    private static double[] AverageOFArraySliding(int[] array, int k)
+    {
+        // the number of sums is array.length - K
+        double[] averages = new double[array.Length - k + 1];
+
+        // calculate sum of first K elements
+        int sum =0;
+        int indexToRemove = -k;
+
+        for (int i=0, j=0; i<array.Length; i++, indexToRemove++)
+        {
+            // add the current number
+            sum+= array[i];
+
+            // if we are at a new sub string 
+            // then move the window and calculate
+            if (i>=k-1)
+            {
+                //remove the item outside of the window
+                if (indexToRemove >0)
+                {
+                    sum -= array[indexToRemove];
+                }
+                //calculate and add the average
+                averages[i-k+1] = (double) sum/k;
+            }
+    
+        }
+        return averages;
     }
 
     private static double[] AverageOFArrayReuse(int[] array, int k)
@@ -33,7 +64,7 @@
         }
         return averages;
     }
-    
+
     private static double[] AverageOFArray(int[] array, int k)
     {
         // the number of sums is array.length - K
