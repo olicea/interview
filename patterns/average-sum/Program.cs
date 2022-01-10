4fa6ca5737e8e791a2cc983e416be841430ae132
Octavio Licea Leon <olicea@github.com>
2022-01-10 07:03:10 -0500

    adding sliding window

--- patterns/average-sum/Program.cs
+++ patterns/average-sum/Program.cs
@@ -5,10 +5,35 @@
         int[] array = new[] {1, 3, 2, 6, -1, 4, 1, 8, 2};
         int k=5;
 
-        double[] averages = AverageOFArray(array, k);
+        double[] averages = AverageOFArrayReuse(array, k);
         Console.WriteLine($"average {string.Join(",", averages)}");
     }
 
+    private static double[] AverageOFArrayReuse(int[] array, int k)
+    {
+        // the number of sums is array.length - K
+        double[] averages = new double[array.Length - k + 1];
+
+        // calculate sum of first K elements
+        int sum =0;
+        for (int j=0; j<k; j++)
+        {
+            sum+= array[j];
+        }
+        averages[0] = (double) sum/k;
+
+        for (int i=1; i<averages.Length; i++)
+        {
+            //subtract old element 
+            sum -= array[i-1];
+            sum += array[i+k-1];
+
+            // store the average of the sub array, force a cast
+            averages[i] = (double) sum/k;
+        }
+        return averages;
+    }
+    
     private static double[] AverageOFArray(int[] array, int k)
     {
         // the number of sums is array.length - K
